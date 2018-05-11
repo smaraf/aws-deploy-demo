989d3bf642ac0dcbbf113a4aafb503c27ae1794d
smaraf <smaranda.onutu@gmail.com>
2018-05-11 08:56:53 -0400

    Scripted lambda deploy pipeline

---
+++ code-pipeline/lambda-deploy-pipeline/create.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#################
+# README FIRST! #
+#################
+# - The pipeline.json need a correct OAUTHTOKEN in order for the GitHub connection to work. 
+# An alternative is to set it up in the UI after it has been created. Webhooks can only be crated
+# through the UI ;(
+# - This script is assuming the region is configured globally using aws configure.
+ 
+ {
+   aws codepipeline create-pipeline --cli-input-json file://pipeline.json
+ } ||
+ { 
+   aws codepipeline update-pipeline --cli-input-json file://pipeline.json
+ }
+
+# Use the following command to get the details of a pipeline:
+# aws codepipeline get-pipeline --name redshift-deploy-pipeline
+
+#TODO: - use secure value for OAUTH token
+#TODO: Script assumes s3 bucket, IAM roles exist. Create cloud formation script for setting up
+# prereqs and their 
